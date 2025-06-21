e2bb668bd04f74ac3cef2619fe8e39c74c3eaf79
Yaman Albezreh <101959672+yamanalbezrah@users.noreply.github.com>
2025-06-21 14:27:28 -0400

    fix hhtp to http

--- backend/main.py
+++ backend/main.py
@@ -11,7 +11,7 @@ app = FastAPI(title="Weather Data System", version="1.0.0")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["hhtp://localhost:3000"],
+    allow_origins=["http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
