d66eadb497c116f7f97086ad0f5b8a0c752c3663
Yaman Albezreh <101959672+yamanalbezrah@users.noreply.github.com>
2025-06-21 14:36:26 -0400

    frontend fixes

--- frontend/src/app/WeatherLookup.tsx
+++ frontend/src/app/WeatherLookup.tsx
@@ -1,14 +1,14 @@
 import { useState } from "react";
 
 export default function WeatherLookup() {
-  const [weatherId, setWeatherID] = useState("");
+  const [weatherId, setWeatherId] = useState("");
   const [data, setData] = useState(null);
   const [error, setError] = useState("");
 
   const fetchWeather = async () => {
     setError("");
     try {
-      const res = await fetch('http://localhost:8000/weather/${weatherID}');
+      const res = await fetch(`http://localhost:8000/weather/${weatherId}`);
       if (!res.ok) throw new Error("Data not found");
       const json = await res.json();
       setData(json);
@@ -22,18 +22,18 @@ return (
   <div>
     <h2>Look Up Weather Data</h2>
     <input 
-      value={weatherID}
-      oneChange={(e) => setWeatherId(e.target.value)}
+      value={weatherId}
+      onChange={(e) => setWeatherId(e.target.value)}
       placeholder="Enter weather ID"
     />
-    <button onClick={fetchWeather}>Fecth</button>
+    <button onClick={fetchWeather}>Fetch</button>
     {error && <p style={{ color: "red" }}>{error}</p>}
 
     {data && (
       <div> 
         <h3>Weather Data</h3>
         <p><strong>Date:</strong> {data.date}</p>
-        <p><strong>Date:</strong> {data.location}</p>
+        <p><strong>Location:</strong> {data.location}</p>
         <p><strong>Notes:</strong> {data.notes}</p>
         <p><strong>Temperature:</strong> {data.weather_data.temperature}°C</p>
         <p><strong>Description:</strong> {data.weather_data.description}</p>
